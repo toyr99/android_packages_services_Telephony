65d45d6ab556e5fe823b5ece0a3d06fdcee379f1
Lars Greiss <kufikugel@googlemail.com>
2014-01-10 12:54:10 +0100

    Telephony: Incoming Call in Background (2/3)
    Patch ported from 4.2 which was done by our lovely HCP. A
    very popular one so we bring it back and adapt it to 4.4
    
    Allows user to set that Incoming Call UI will remain in background
    if screen is on. Instead, a notification is added to StatusBar, which
    can be used to bring up the Incoming Call UI. Ringtone and vibration
    still occur as per system settings.
    
    This has the same effect as apps marked as "immersive". It prevents
    the incoming call from backgrounding the current activity.
    
    PS: reorder settings a bit
    
    Change-Id: I159bd817ebcc39a0733f9433feb29b310adbc2aa

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -190,6 +190,8 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String BUTTON_GSM_UMTS_OPTIONS = "button_gsm_more_expand_key";
     private static final String BUTTON_CDMA_OPTIONS = "button_cdma_more_expand_key";
 
+    private static final String BUTTON_CALL_UI_IN_BACKGROUND = "bg_incall_screen";
+
     private static final String VM_NUMBERS_SHARED_PREFERENCES_NAME = "vm_numbers";
 
     private static final String BUTTON_SIP_CALL_OPTIONS =
@@ -282,6 +284,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private CheckBoxPreference mPlayDtmfTone;
     private CheckBoxPreference mButtonAutoRetry;
     private CheckBoxPreference mButtonHAC;
+    private CheckBoxPreference mButtonCallUiInBackground;
     private ListPreference mButtonDTMF;
     private ListPreference mButtonTTY;
     private CheckBoxPreference mButtonNoiseSuppression;
@@ -516,6 +519,7 @@ public class CallFeaturesSetting extends PreferenceActivity
             // Update Noise suppression value in Settings database
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     Settings.System.NOISE_SUPPRESSION, nsp);
+        } else if (preference == mButtonCallUiInBackground) {
             return true;
         } else if (preference == mButtonAutoRetry) {
             android.provider.Settings.Global.putInt(mPhone.getContext().getContentResolver(),
@@ -588,6 +592,10 @@ public class CallFeaturesSetting extends PreferenceActivity
                     Settings.System.DTMF_TONE_TYPE_WHEN_DIALING, index);
         } else if (preference == mButtonTTY) {
             handleTTYChange(preference, objValue);
+        } else if (preference == mButtonCallUiInBackground) {
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.CALL_UI_IN_BACKGROUND,
+                    (Boolean) objValue ? 1 : 0);
         } else if (preference == mMwiNotification) {
             int mwi_notification = mMwiNotification.isChecked() ? 1 : 0;
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
@@ -1582,6 +1590,8 @@ public class CallFeaturesSetting extends PreferenceActivity
         mButtonHAC = (CheckBoxPreference) findPreference(BUTTON_HAC_KEY);
         mButtonTTY = (ListPreference) findPreference(BUTTON_TTY_KEY);
         mButtonNoiseSuppression = (CheckBoxPreference) findPreference(BUTTON_NOISE_SUPPRESSION_KEY);
+        mButtonCallUiInBackground =
+                (CheckBoxPreference) findPreference(BUTTON_CALL_UI_IN_BACKGROUND);
         mVoicemailProviders = (ListPreference) findPreference(BUTTON_VOICEMAIL_PROVIDER_KEY);
         mButtonBlacklist = (PreferenceScreen) findPreference(BUTTON_BLACKLIST);
         mFlipAction = (ListPreference) findPreference(FLIP_ACTION_KEY);
@@ -1661,6 +1671,10 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         if (mFlipAction != null) {
             mFlipAction.setOnPreferenceChangeListener(this);
+        }
+
+        if (mButtonCallUiInBackground != null) {
+            mButtonCallUiInBackground.setOnPreferenceChangeListener(this);
         }
 
         if (!getResources().getBoolean(R.bool.world_phone)) {
@@ -1889,6 +1903,12 @@ public class CallFeaturesSetting extends PreferenceActivity
                     Settings.System.CALL_FLIP_ACTION_KEY, 2);
             mFlipAction.setValue(String.valueOf(flipAction));
             updateFlipActionSummary(flipAction);
+        }
+
+        if (mButtonCallUiInBackground != null) {
+            int callUiInBackground = Settings.System.getInt(getContentResolver(),
+                    Settings.System.CALL_UI_IN_BACKGROUND, 0);
+            mButtonCallUiInBackground.setChecked(callUiInBackground != 0);
         }
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
